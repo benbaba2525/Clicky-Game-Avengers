9fa8a6d07557af081379d96408d39d47d7d21283
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-08-21 00:48:00 -0700

    Updates

---
+++ precache-manifest.b407fbc90bc9c45613afd29f94b8907c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca603680cebaf39fa4348e57ce184e02",
+    "url": "/Clicky-Game-Avengers/index.html"
+  },
+  {
+    "revision": "d3031432b112defa8761",
+    "url": "/Clicky-Game-Avengers/static/css/main.de061b2d.chunk.css"
+  },
+  {
+    "revision": "54ae120f9593b16e8c47",
+    "url": "/Clicky-Game-Avengers/static/js/2.6a885b5e.chunk.js"
+  },
+  {
+    "revision": "0c642dbff179889abda1a42753a27d19",
+    "url": "/Clicky-Game-Avengers/static/js/2.6a885b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3031432b112defa8761",
+    "url": "/Clicky-Game-Avengers/static/js/main.d29f94be.chunk.js"
+  },
+  {
+    "revision": "6cb13bc50f284c200281",
+    "url": "/Clicky-Game-Avengers/static/js/runtime-main.ed2395ae.js"
+  },
+  {
+    "revision": "1765eddf8e86d150b37c7fb1417bd2e8",
+    "url": "/Clicky-Game-Avengers/static/media/You Lose - Sound Effect - 2017.1765eddf.mp3"
+  },
+  {
+    "revision": "c7551b0560cf795709d3696c2d68e41f",
+    "url": "/Clicky-Game-Avengers/static/media/You Win (Voice) - Sound Effect [HD].c7551b05.mp3"
+  },
+  {
+    "revision": "ecf83eac8834d9bd1c1c5d110e14b80b",
+    "url": "/Clicky-Game-Avengers/static/media/correct sound effect.ecf83eac.mp3"
+  },
+  {
+    "revision": "7e7c97af850fe70c8d1cd2eefaac5429",
+    "url": "/Clicky-Game-Avengers/static/media/maxresdefault.7e7c97af.jpg"
+  }
+]);
